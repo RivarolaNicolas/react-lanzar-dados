2c24d51bfe306d843e5d3717d41f57f1c90e7499
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-10-21 13:53:12 -0300

    Updates

---
+++ precache-manifest.fb9b04f599c2b098ad35a928d443bc60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fd490e4f771d798745a999dcae0d3e",
+    "url": "/react-lanzar-dados/index.html"
+  },
+  {
+    "revision": "600555337a3297c13eaa",
+    "url": "/react-lanzar-dados/static/css/main.59b75110.chunk.css"
+  },
+  {
+    "revision": "4098b591cd77157ac720",
+    "url": "/react-lanzar-dados/static/js/2.44cfe492.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-lanzar-dados/static/js/2.44cfe492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600555337a3297c13eaa",
+    "url": "/react-lanzar-dados/static/js/main.913adca1.chunk.js"
+  },
+  {
+    "revision": "7bd67ff46e1030a49125",
+    "url": "/react-lanzar-dados/static/js/runtime-main.720c8165.js"
+  }
+]);
